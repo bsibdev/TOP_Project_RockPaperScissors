a2c1949a44c6008f0e6aaff82aab504a32d37f2b
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-03-25 10:25:29 -0400

    create rough draft

--- rps.js
+++ rps.js
@@ -1,9 +1,54 @@
-function getComputerChoice(){}
-
+//ask player for rock, paper, or scissors
 function getPlayerChoice(){
-    let playerChoice = promt("Choose Wisely")
+    let playerChoice = promt("Choose Wisely");
 }
 
-function playRound(playerChoice,computerChoice) {
+//computer picks rock, paper, or scissors
+function getComputerChoice(){
+    let randomInt = Math.floor(Math.random() * 3)
+    let computerChoice
+    
+    if (randomInt <2) {
+        computerChoice = "Rock";
 
+    } else if (randomInt > 2 && randomInt <3) {
+        computerChoice = "Paper";
+
+    } else {computerChoice = "Scissors"}
+}
+
+
+//player and computer choices compared to determine winner
+function playRound(playerChoice,computerChoice) {
+    if (playerChoice == computerChoice) {
+        return "Draw";
+
+    } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
+        return "Winner";
+
+    } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
+        return "Winner";
+
+    } else if (playerChoice == "Paper" && computerChoice == "Rock") {
+        return "Winner";
+
+    } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
+        return "Loser";
+        
+    } else if (playerChoice == "Scissors" && computerChoice == "Rock") {
+        return "Loser";
+
+    } else if (playerChoice == "Paper" && computerChoice == "Scissors") {
+        return "Loser";
+
+    } else {
+        return "Try Again";
+    }
+    
+}
+
+function playGame () {
+    getPlayerChoice()
+    getComputerChoice()
+    playRound();
 }
